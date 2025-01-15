b07add81602995b64aa13046a8c138bace365912
Andrew <aldortheold@gmail.com>
2025-01-15 15:16:36 +1000

    added buttons for exporting and importing themes

--- src/FontSettings.jsx
+++ src/FontSettings.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import text from './assets/json/text.json';
+import { motion } from 'framer-motion';
 
 function FontSettings({ settings, setSettings }) {
     return (
-        <p className="p1">this section is in progress!</p>
+        <motion.div
+            className='fonts'
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.5}}
+        >
+            <p className="p1">this section is in progress!</p>
+        </motion.div>
     )
 }
 
--- src/SavedThemes.jsx
+++ src/SavedThemes.jsx
@@ -59,7 +59,18 @@ function SavedThemes({ settings, setSettings, savedList, setSavedList }) {
                     )
                 }</div>
                 : <NoThemes settings={settings} />}
-                <div style={{height: "1rem"}}/>
+                <div style={{height: "0.5rem"}}/>
+                <div className="collection-actions">
+                    <button>
+                        <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="var(--button-font)" strokeWidth="4.48"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" strokeWidth="5.376"><polyline points="48 24 32 8 16 24"></polyline><line x1="56" y1="56" x2="8" y2="56"></line><line x1="32" y1="48" x2="32" y2="8"></line></g><g id="SVGRepo_iconCarrier"><polyline points="48 24 32 8 16 24"></polyline><line x1="56" y1="56" x2="8" y2="56"></line><line x1="32" y1="48" x2="32" y2="8"></line></g></svg>
+                        export themes
+                    </button>
+                    <button>
+                        <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="var(--button-font)" strokeWidth="4.48"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" strokeWidth="5.376"><polyline points="16 32 32 48 48 32"></polyline><line x1="56" y1="56" x2="8" y2="56"></line><line x1="32" y1="8" x2="32" y2="48"></line></g><g id="SVGRepo_iconCarrier"><polyline points="16 32 32 48 48 32"></polyline><line x1="56" y1="56" x2="8" y2="56"></line><line x1="32" y1="8" x2="32" y2="48"></line></g></svg>
+                        import themes
+                    </button>
+                </div>
+                <div style={{height: "1.5rem"}}/>
                 <a onClick={() => navigate("/settings")}>{text[settings.language].back}</a>
                 <div style={{height: "2rem"}} />
             </>
