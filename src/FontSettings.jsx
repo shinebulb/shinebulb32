c143c8d6eeba52cce0b70e4a40fa42b80045b96b
Andrew <aldortheold@gmail.com>
2025-01-15 14:43:01 +1000

    created a page for advanced font settings

---
+++ src/FontSettings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FontSettings({ settings, setSettings }) {
+    return (
+        <p className="p1">this section is in progress!</p>
+    )
+}
+
+export default FontSettings
