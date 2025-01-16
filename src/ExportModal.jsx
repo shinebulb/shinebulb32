5977a9df1568391575036259895457d16c0271e3
Andrew <aldortheold@gmail.com>
2025-01-16 20:28:01 +1000

    improved styles of the export-options buttons

--- src/ExportModal.jsx
+++ src/ExportModal.jsx
@@ -21,7 +21,11 @@ function ExportModal({ exportModal, settings, savedList }) {
         link.download = "shinebulb-themes.json";
         link.click();
         URL.revokeObjectURL(url);
-    }; 
+    }
+
+    function copyThemes() {
+
+    }
 
     return (
         <dialog ref={exportModal} className="export-modal">
@@ -55,11 +59,11 @@ function ExportModal({ exportModal, settings, savedList }) {
                 >⟫</button>
             </div>
             <div className="export-options">
-                <button onClick={downloadThemes}>
+                <button onClick={downloadThemes} disabled={themeExport.length == 0}>
                     <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="var(--button-font)" strokeWidth="4.48"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" strokeWidth="5.376"><polyline points="16 32 32 48 48 32"></polyline><line x1="56" y1="56" x2="8" y2="56"></line><line x1="32" y1="8" x2="32" y2="48"></line></g><g id="SVGRepo_iconCarrier"><polyline points="16 32 32 48 48 32"></polyline><line x1="56" y1="56" x2="8" y2="56"></line><line x1="32" y1="8" x2="32" y2="48"></line></g></svg>
                     {text[settings.language].exportModal[1]}
                 </button>
-                <button>
+                <button onClick={copyThemes} disabled={themeExport.length == 0}>
                     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d={paths.copy[0]} fill="var(--button-font)"></path><path d={paths.copy[1]} fill="var(--button-font)"></path></g></svg>
                     {text[settings.language].exportModal[2]}
                 </button>
