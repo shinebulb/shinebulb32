73462507feca79a91ee6b18c7bc57f04abe2e68e
Andrew <aldortheold@gmail.com>
2025-01-17 16:29:51 +1000

    added basic structure for the import modal

--- src/ImportModal.jsx
+++ src/ImportModal.jsx
@@ -7,6 +7,11 @@ function ImportModal({ importModal, settings, savedList, setSavedList }) {
     return (
         <dialog ref={importModal} className="export-modal">
             <p className="export-title">import themes</p>
+            <hr style={{marginBottom: "0.8rem"}} />
+            <p>paste the code containing themes you want to import here:</p>
+            <textarea />
+            <p>or upload it from a .json file:</p>
+            <button>upload themes</button>
             <hr />
             <button id="close-export-modal" onClick={() => importModal.current.close()}>{text[settings.language].close}</button>
         </dialog>
