189250fa464a5807d4e38e7936ac8b157287818c
Andrew <aldortheold@gmail.com>
2025-02-23 11:47:07 +1000

    updated animation speed on app load

--- src/AppLoader.jsx
+++ src/AppLoader.jsx
@@ -7,7 +7,7 @@ function AppLoader() {
             <img src="img/off.svg" height="200" className="background-image" />
             <div 
                 className="overlay-container" 
-                style={{ animation: "revealAnimation 3000ms linear infinite" }}
+                style={{ animation: "revealAnimation 1500ms linear infinite" }}
             >
                 <img src="img/on.svg" height="200" className="overlay-image" />
             </div>
