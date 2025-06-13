a025bec2fe7d30fe87526a75de6caa51cc2796cc
Andrew <aldortheold@gmail.com>
2025-06-13 21:35:54 +1000

    added media query to the new devpage layout

--- src/DevPage.jsx
+++ src/DevPage.jsx
@@ -21,8 +21,9 @@ function DevPage({ settings }) {
             exit={{opacity: 0}}
             transition={{duration: 0.5}}
         >
+            <div style={{height: "2rem"}}/>
             <p className="version">v{version}</p>
-            <p className="p3" style={{fontSize: "1.7rem", fontWeight: "bold", margin: "1rem 0"}}>{text[settings.language].devSides[0]}</p>
+            <p className="p3" style={{fontWeight: "bold", margin: "1rem 0"}}>{text[settings.language].devSides[0]}</p>
             <RepoMenu />
             <div style={{height: "2rem"}}/>
             <a onClick={() => navigate("/")}>{text[settings.language].back}</a>
