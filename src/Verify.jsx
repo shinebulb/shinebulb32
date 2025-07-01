8abd659865873edfa9e69dd0fdaa876309f6ed07
Andrew <aldortheold@gmail.com>
2025-07-01 22:47:51 +1000

    advanced email verification ux

--- src/Verify.jsx
+++ src/Verify.jsx
@@ -1,48 +1,46 @@
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import text from './assets/json/text.json';
+import { motion } from 'framer-motion';
 
 function Verify({ settings }) {
 
     const { search } = useLocation();
     const navigate = useNavigate();
-    const [status, setStatus] = useState('verifying your account...');
+    const [status, setStatus] = useState(0);
 
     useEffect(() => {
         const params = new URLSearchParams(search);
         const token = params.get('token');
 
         if (!token) {
-            setStatus('invalid or missing verification token.');
+            setStatus(1);
             return;
         }
 
         axios.get(`${import.meta.env.VITE_API_KEY}/users/verify?token=${token}`)
-        .then(response => {
-            if (!response.data.error) {
-                setStatus('email verified successfully!');
-            }
-            else {
-                setStatus('verification failed or link expired. please try again.');
-            }
-        })
-        .catch(() => {
-            setStatus('verification failed or link expired. please try again.');
-        });
+        .then(response => setStatus(Number(response.data.status)))
+        .catch(() => setStatus(2));
     }, [search, navigate]);
 
     return (
-        <div>
-            <p style={{ color: "var(--font)" }}>{status}</p>
-            {status.toLowerCase().includes('failed') && (
-            <button
-                style={{ backgroundColor: "var(--button-bg)", color: "var(--button-font)" }}
-                onClick={() => window.location.reload()}
-            >
-                Retry
-            </button>
-            )}
-        </div>
+        <motion.div
+            className='verify'
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.5}}
+        >
+            <p>{text[settings.language].verifyStatus[status]}</p>
+            {status === 1 || status === 2 ?
+            <button onClick={() => window.location.reload()}>
+                retry
+            </button> :
+            <button onClick={() => navigate("/")}>
+                exit
+            </button>}
+        </motion.div>
     );
 }
 
