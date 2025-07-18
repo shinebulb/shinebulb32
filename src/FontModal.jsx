895d72051b0575a3fa769de3aa97e32c24ba8fc7
Andrew <aldortheold@gmail.com>
2025-07-18 15:03:56 +1000

    made all default fonts support cyrillic

--- src/FontModal.jsx
+++ src/FontModal.jsx
@@ -86,7 +86,7 @@ function FontModal({ modal, settings, setSettings, width }) {
                         {text[settings.language].fontOptions[1]}
                     </label>
                     <div className="font-editor" style={{opacity: preferred === 'default' ? 1 : 0.5, pointerEvents: preferred === 'default' ? 'auto' : 'none'}}>
-                        <select value={font} onChange={(e) => setFont(e.target.value)} disabled={preferred !== 'default'} size={Math.min(fonts.length, 4)}>
+                        <select value={font} onChange={(e) => setFont(e.target.value)} disabled={preferred !== 'default'} size={Math.min(fonts.length, 3)}>
                             {fonts.map((value, index) => (
                                 <option key={index} value={value} style={{fontFamily: value, backgroundColor: value == font ? "var(--font)" : "transparent", color: value == font ? "var(--modal-bg)" : "var(--font)"}}>
                                     {value}
