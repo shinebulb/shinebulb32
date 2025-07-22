781c6bff51098633add7329d7776b6833f00d76c
Andrew <aldortheold@gmail.com>
2025-07-22 12:56:55 +1000

    styled delete-font modal

--- src/FontCard.jsx
+++ src/FontCard.jsx
@@ -48,16 +48,16 @@ function FontCard({ id, fontFamily, settings, setSettings, fontList, setFontList
                     : <svg xmlns="http://www.w3.org/2000/svg" onClick={applyFont} viewBox="0 -960 960 960"><title>{text[settings.language].fontCard[0]}</title><path d={paths.applyFont}/></svg>}
                     <svg xmlns="http://www.w3.org/2000/svg" onClick={() => deleteRef.current.showModal()} viewBox="0 -960 960 960"><title>{text[settings.language].fontCard[1]}</title><path d={paths.delete}/></svg>
                 </div>
-            </div>
 
-            <dialog closedby="any" ref={deleteRef} disabled={loadDelete} className="confirm">
-                <p>{text[settings.language].deleteFont.replace("font", `"${fontFamily}"`)}</p>
-                <button onClick={deleteFont}>{
-                    loadDelete ? <span className="loader" style={{ width: "1rem", height: "1rem" }} />
-                    : text[settings.language].confirm[1]
-                }</button>
-                <button onClick={() => closeModal(deleteRef)}>{text[settings.language].confirm[2]}</button>
-            </dialog>
+                <dialog closedby="any" ref={deleteRef} disabled={loadDelete} className="confirm">
+                    <p style={{ fontFamily: fontFamily }}>{text[settings.language].deleteFont}</p>
+                    <button onClick={deleteFont} style={{ fontFamily: fontFamily }}>{
+                        loadDelete ? <span className="loader" style={{ width: "1rem", height: "1rem" }} />
+                        : text[settings.language].confirm[1]
+                    }</button>
+                    <button onClick={() => closeModal(deleteRef)} style={{ fontFamily: fontFamily }}>{text[settings.language].confirm[2]}</button>
+                </dialog>
+            </div>
         </>
     )
 }
