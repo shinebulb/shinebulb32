f760e0f8081d5d6bde1e7fbc24c5cc76329917a9
Andrew <aldortheold@gmail.com>
2025-08-22 20:43:47 +1000

    added profile links to /users

--- src/UsersCard.jsx
+++ src/UsersCard.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import text from './assets/json/text.json';
 
 function UsersCard({ user }) {
+
+    const navigate = useNavigate();
+
     return (
-        <div>{user.username}</div>
+        <div className="user-card">
+            <a onClick={() => navigate(`/user/${user.username}`)}>{user.username}</a>
+        </div>
     )
 }
 
